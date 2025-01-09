a02fbe2ca3df2affe6c82dc7b90d9fae1df9cbce
Darshini <darshinivellaichamy@gmail.com>
2025-01-09 14:15:11 +0530

    ola comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './TrainerComponent/home/home.component';
 import { AsideComponent } from './TrainerComponent/aside/aside.component';
 import { MarksComponent } from './TrainerComponent/marks/marks.component';
 import { CourseComponent } from './TrainerComponent/course/course.component';
+import { OlaComponent } from './ola/ola.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent},
     {path:"home",component:HomeComponent},
     {path:"aside",component:AsideComponent},
     {path:"marks",component:MarksComponent},
-    {path:"course",component:CourseComponent}
+    {path:"course",component:CourseComponent},
+    {path:"ola",component:OlaComponent}
 
 ];
