20304f496dbb37aa6c21afa3307ce53dec599f6c
dhanalakshmi <dhanalakshmik726@gmail.com>
2025-01-10 10:32:14 +0530

    updated manager component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { HomeComponent } from './TrainerComponent/home/home.component';
 import { AsideComponent } from './TrainerComponent/aside/aside.component';
 import { MarksComponent } from './TrainerComponent/marks/marks.component';
 import { CourseComponent } from './TrainerComponent/course/course.component';
-import { ChecklistComponent } from './TrainerComponent/Component/checklist/checklist.component';
-import { LoginComponentComponent } from './TrainerComponent/Component/login-component/login-component.component';
-import { RoadmapComponent } from './TrainerComponent/Component/roadmap/roadmap.component';
+import { ChecklistComponent } from './TrainerComponent/checklist/checklist.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { RoadmapComponent } from './Manager/roadmap/roadmap.component';
+
 
 export const routes: Routes = [
     {path:"",component:HomeComponent},
