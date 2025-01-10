c7b655dbe6f232ed29da410434499fd4032bf111
dhanalakshmi <dhanalakshmik726@gmail.com>
2025-01-10 11:05:20 +0530

    updated manager

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,16 +6,29 @@ import { CourseComponent } from './TrainerComponent/course/course.component';
 import { ChecklistComponent } from './TrainerComponent/checklist/checklist.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
 import { RoadmapComponent } from './Manager/roadmap/roadmap.component';
+import { ManagerDashboardComponent } from './Manager/manager-dashboard/manager-dashboard.component';
+import { StudentDetailsComponent } from './Manager/student-details/student-details.component';
+import { CourseDetailsComponent } from './Manager/course-details/course-details.component';
+import { TrainersComponent } from './Manager/trainers/trainers.component';
+import { PublishMarksComponent } from './Manager/publish-marks/publish-marks.component';
+import { InsightsComponent } from './Manager/insights/insights.component';
 
 
 export const routes: Routes = [
-    {path:"",component:HomeComponent},
+    {path:"",component:LoginComponentComponent},
     {path:"home",component:HomeComponent},
     {path:"aside",component:AsideComponent},
     {path:"marks",component:MarksComponent},
     {path:"course",component:CourseComponent},
     {path:"check",component:ChecklistComponent},
     {path:"login",component:LoginComponentComponent},
-    {path:"timeline",component:RoadmapComponent}
+    {path:"timeline",component:RoadmapComponent},
+    {path:"manager",component:ManagerDashboardComponent},
+    {path:"student-details",component:StudentDetailsComponent},
+    {path:"course-details",component:CourseDetailsComponent},
+    {path:"Trainers",component:TrainersComponent},
+    {path:"PublishMarks",component:PublishMarksComponent},
+    {path:"Insights",component:InsightsComponent},
+    
 
 ];
