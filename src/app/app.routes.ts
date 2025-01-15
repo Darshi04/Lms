eee035c0cb0f61feed60c3ebf1290e9cb6365036
arun <arunvenkatesh841@gmail.com>
2025-01-15 10:00:44 +0530

    dashboard connect to login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,7 @@ import { CourseDetailsComponent } from './Manager/course-details/course-details.
 import { TrainersComponent } from './Manager/trainers/trainers.component';
 import { PublishMarksComponent } from './Manager/publish-marks/publish-marks.component';
 import { InsightsComponent } from './Manager/insights/insights.component';
+import { DashboardComponent } from './trainee/dashboard/dashboard.component';
 
 
 export const routes: Routes = [
@@ -29,6 +30,7 @@ export const routes: Routes = [
     {path:"Trainers",component:TrainersComponent},
     {path:"PublishMarks",component:PublishMarksComponent},
     {path:"Insights",component:InsightsComponent},
+    {path:"dashboard",component:DashboardComponent}
     
 
 ];
